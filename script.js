4b67720b0a35ca68ab25b48e61f03ced156b0f62
Kazi Robin <kazirobinking@gmail.com>
2023-10-04 21:14:21 +0600

    update(4/10/23 )

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+const navbar = document.querySelector('.navbar');
+window.addEventListener('scroll',(event) => {
+    if(this.scrollY>100){
+        navbar.classList.add('bg__color')
+    }
+    else{
+        navbar.classList.remove('bg__color')
+
+    }
+})
 const bar = document.querySelector('.navbar__bar');
 const menu = document.querySelector('.navbar__menu');
 bar.addEventListener('click',() => {
