f34e961abd5d7727d82f0f191aedd46a5d01a7ff
Kazi Robin <kazirobinking@gmail.com>
2023-10-04 23:31:08 +0600

    update again(4/10/23)

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
-const navbar = document.querySelector('.navbar');
-window.addEventListener('scroll',(event) => {
-    if(this.scrollY>100){
-        navbar.classList.add('bg__color')
-    }
-    else{
-        navbar.classList.remove('bg__color')
+const navbar = document.querySelector(".navbar");
+window.addEventListener("scroll", (event) => {
+  if (this.scrollY > 100) {
+    navbar.classList.add("bg__color");
+  } else {
+    navbar.classList.remove("bg__color");
+  }
+});
+const bar = document.querySelector(".navbar__bar");
+const barIcon = document.querySelector(".navbar__bar__icon");
+const menu = document.querySelector(".navbar__menu");
+bar.addEventListener("click", () => {
+  menu.classList.toggle("active");
+  barIcon.classList.toggle("active");
 
-    }
-})
-const bar = document.querySelector('.navbar__bar');
-const menu = document.querySelector('.navbar__menu');
-bar.addEventListener('click',() => {
-    menu.classList.toggle('active')
-})
+});
